3900917a1086505151be35ac05850d3ec95fea94
Mr Jones <github@kjones.id.au>
2017-08-14 19:14:47 +1000

    Added new Story

---
+++ Testing/EmotionalModeling/stories/TheCostofCowardice.py
@@ -0,0 +1,89 @@
+# The Farmer and the Warrior
+
+""" 
+positive and negative are valenced reactions (to “something”)
+pleased is being positive about a consequence (of an event)
+displeased is being negative about a consequence (of an event)
+hope is being pleased about a prospective consequence (of an event)
+fear is being displeased about a prospective consequence (of an event)
+joy is being pleased about an actual consequence (of an event)
+distress is being displeased about an actual consequence (of an event)
+
+satisfaction is joy about the confirmation of a prospective desirable consequence
+fears-confirmed is distress about the confirmation of a prospective undesirable consequence
+relief is joy about the disconfirmation of a prospective undesirable consequence
+disappointment is distress about the disconfirmation of a prospective desirable consequence
+
+
+happy-for is joy about a consequence (of an event) presumed to be desirable for someone else
+resentment is distress about a consequence (of an event) presumed to be desirable for someone else
+gloating is joy about a consequence (of an event) presumed to be undesirable for someone else
+pity is distress about a consequence (of an event) presumed to be undesirable for someone else
+
+approving is being positive about an action (of an source)
+disapproving is being negative about an action (of an source)
+pride is approving of one’s own action
+shame is disapproving of one’s own action
+admiration is approving of someone else’s action
+reproach is disapproving of someone else’s action
+gratification is pride about an action and joy about a related consequence
+remorse is shame about an action and distress about a related consequence
+gratitude is admiration about an action and joy about a related consequence
+anger is reproach about an action and distress about a related consequence
+liking is being positive about an aspect (of an object)
+disliking is being negative about an aspect (of an object)
+love is liking a familiar aspect (of an object)
+hate is disliking a familiar aspect (of an object)
+interest is liking an unfamiliar aspect (of an object)
+disgust is disliking an unfamiliar aspect (of an object)
+
+A farmer wanders cautiously through the forest wary of the hostile surroundings. But desperate times called for desperate measures, and so he was foraging for food for his family.
+
+Farmer:
+-ve Feeling @ Chance of Danger = Fear(-,+,-) # Being a weak farmer, he has a greater fear of danger/death/pain than a warrior (worldView)
+--ve Feeling @ Chacne of Death(Family) = Bigger Fear(-,+,-) # The survival of his family is more important than his own safety
+
+
+As the farmer forages he happens upon a free spirited warrior in a clearing. The warrior seems unconcerned about his dangerous surroundings, and is quite pleased with himself.
+Warrior:
+-ve Feeling @ Chance of Danger (but far less than the farmer because our warrior fears it less (worldview on danger))
++ve Feeling @ Chance of Battle (Our Warrior loves the thrill of battle. He believes the greatest things in life are to to crush your enemies, see them driven before you and hear the lamentations of their women. )
+
+Before either can act, the sudden silence is broken by a crashing of brush and a hellish roar. The Dread Seeker enters the clearing.
+
+Dread Seeker:
+-ve Feeling @ Chance of Danger = Fear
++ve Feeling @ Chance of Food = Hope
+He's hungry, the feelings about food are heightened and override fear.
+
+The warrior jumps to his feet and grabs his longsword. Calling out to the farmer he yells triumphantly. "Come, let us slay this foul beast"
+Warrior:
++ve Feeling about Battle = Approving
++ve Feeling about Chance of Conquest = Hope
+-ve Feeling about Pain = Fear
+-ve Feeling about Death = Fear
+# But our warrior values conquest too highly and his fear of pain and death are dampened by his worldView (he value pain and death so lowly)
+
+The farmer on the other hand is terrified.
+-ve Feeling about Battle = Approving
+-ve Feeling about Chance of Death = Fear
+-ve Feeling about Chance of Pain = Fear
++ve Feeling about Chance of Conquest = Hope
+# Not on your life sir, I'm fleeing.
+
+The Farmer abandons the Warrior to his battle, and flees into the forest.
+Farmer:
+-ve Disapproves Self Abandoning Ally = Shame
+-ve Feeling about Chance of Death = Fear
+-ve Feeling about Chance of Pain = Fear
+He feels ashamed that he's leaving the warrior to his doom. But his fear overrides his actions and he continues to flee.
+
+The Warrior snorts in disgust at the farmers cowardice.
+-ve Dissapproves Farmers Abandoning = Reproach
+-ve Feeling about Chance of Death = Fear
+-ve Feeling about Chance of Pain = Fear
++ve Feeling about Chance of Conquest
+The warrior turns to his foe and says, "come, lets dance"
+
+.. etc. etc. This continues on. As the warrior fights his fear of death will increase as it's chance increases, likewise if he's winning, then his chance of conquest continues to rise too. 
+So if they are evenly matched, it could easily go down to the wire
