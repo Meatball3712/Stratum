5da2757712b5b5b008261f3d96c1c986deb31688
Mr Jones <github@kjones.id.au>
2017-08-22 21:20:40 +1000

    Removed debug print statement.

--- Testing/EmotionalModeling/world/wardentown.py
+++ Testing/EmotionalModeling/world/wardentown.py
@@ -5,5 +5,4 @@ from world.biomes import Village
 class WardenTown(Village):
     """docstring for WardenTown"""
     def __init__(self, **kwargs):
-        print(kwargs)
         super().__init__(name="Warden Town", **kwargs)
