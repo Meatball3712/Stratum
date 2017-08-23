c30269fe119a791b69443c714f568729c182e97b
Mr Jones <github@kjones.id.au>
2017-08-23 19:59:02 +1000

    Moving stuff around.

---
+++ Testing/EmotionalModeling/ai/SituationalAnalysis.py
@@ -0,0 +1,45 @@
+#!python3
+import sys, os
+sys.path.insert(0, os.path.abspath('..'))
+
+import numpy as np
+import pickle
+
+class Situation:
+    
+    location = None
+    actors = None
+    
+    def __init__(self, character, actors, location):
+        """
+        Build the vector representations needed by the predictor
+        """
+        self.location = location.getVector()
+        self.actors = ([np.hstack(
+                            [a.getVector(a is character), 
+                            character.getOpinionVector(a)]) 
+                        for a in actions])
+
+class SituationAnalyser:
+    """
+    SituationAnalyser learns an association of situation -> PAD outcome
+    based on past experiences.
+    """
+    model = None
+    
+    def __init__(self, modelFileName = None):
+        if modelFileName is not None:
+            if isinstance(model, str):
+                self.model = pickle.load(open(modelFileName,'r'))
+            else:
+                self.model = modelFileName
+    
+    def Observe(self, situation, PADDelta):
+        if model is not None:
+            model.update(situation, PADDelta)
+    
+    def Predict(self, situation):
+        if model is not None:
+            return model.predict(situation)
+        return np.array((0.,0.,0.))
+        
